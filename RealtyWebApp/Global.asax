048881b69b7de5ed113f68a54b7f306d5be370b2
Curtis Brock <curtisbrock2@gmail.com>
2018-09-18 21:16:21 -0400

    Initial

---
+++ RealtyWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RealtyWebApp.MvcApplication" Language="C#" %>
---
+++ RealtyWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RealtyWebApp.Startup))]
+namespace RealtyWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
